05e3ce1bee13db07863765124a88bf2d88038068
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-05-16 15:08:06 +0100

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from "lodash.throttle";
+import vimeoPlayer from "@vimeo/player";
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+    const updateTime = seconds =>
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+    
+    player.on('timeupdate', throttle(updateTime, 1000));
+
+    const playedTime = localStorage.getItem("videoplayer-current-time")
+    const currentTime = JSON.parse(playedTime);
+
+    if (currentTime.seconds)
+        {player.setCurrentTime(currentTime.seconds)}
+
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+    
